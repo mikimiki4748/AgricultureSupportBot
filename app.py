3dfea0b00a7b78f8747564c767f64527e6070f86
kohki <tk011875@gmail.com>
2017-08-14 00:37:17 +0900

    edit host name

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ app = Flask(__name__)
 # get channel_secret and channel_access_token from your environment variable
 channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
 channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
-host_name = 'https://peaceful-waters-26053.herokuapp.com'
+host_name = 'https://nameless-gorge-55138.herokuapp.com/'
 
 if channel_secret is None:
     print('Specify LINE_CHANNEL_SECRET as environment variable.')
